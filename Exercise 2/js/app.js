187c99ac0aeb5a35ed8443fa3f08a2ba2f678381
popii87 <76619024+popii87@users.noreply.github.com>
2021-02-02 20:34:44 +0100

    Add files via upload

---
+++ Exercise 2/js/app.js
@@ -0,0 +1,17 @@
+let numbers = [];
+
+
+    
+    
+
+
+
+    for(i = 1; i <=100; i++) {
+        
+        if(i % 7 === 0 && i % 3 === 0){
+            numbers.push(i);
+        }
+    }
+    
+
+ console.log(numbers);
