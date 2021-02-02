5406ee4cf1f8fa04e9182ecaa47473840310ebb6
popii87 <76619024+popii87@users.noreply.github.com>
2021-02-02 20:34:08 +0100

    Add files via upload

---
+++ Exercise 1/js/app.js
@@ -0,0 +1,16 @@
+let namesArray = ['Bill', 'Ana', 'Josephine', 'Elena'];
+let nameslengthArray = [];
+
+
+
+
+    for(i = 0; i < namesArray.length; i++) {
+        let fname = namesArray[i].length;
+        nameslengthArray.push(fname);
+        
+        
+    }
+
+
+console.log(nameslengthArray);
+
