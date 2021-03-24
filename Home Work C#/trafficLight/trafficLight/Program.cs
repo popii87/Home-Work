6fdf182ec1304731f3751fe180825b28d912991c
popii87 <76619024+popii87@users.noreply.github.com>
2021-03-24 20:06:51 +0100

    Add files via upload

---
+++ Home Work C#/trafficLight/trafficLight/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace trafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
